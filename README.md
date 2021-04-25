c28b1aa3e7014dcb5a3e9ca48ec1e12cbf2fce1c
yugsheladiya <82305223+yugsheladiya@users.noreply.github.com>
2021-04-25 11:51:50 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# p5.play-boilerplate
-Boiler plate for p5.play
+# collisionAlgorithm
+Algorithm to detect collision between two objects
--- sketch.js
+++ sketch.js
@@ -1,24 +1,52 @@
-var fixedRect,movingRect;
-function setup() {
-  createCanvas(400,400);
-  fixedRect= createSprite(200, 200, 50, 80);
-  movingRect= createSprite(400, 200, 80, 30);
 
+var fixedRect, movingRect;
+var gameObject1, gameObject2;
+
+function setup() {
+  createCanvas(1200,800);
+ 
+ 
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  fixedRect = createSprite(100, 100, 50, 50);
+  fixedRect.shapeColor = "green";
+  
+  
 }
 
 function draw() {
-  background("black");  
-  movingRect.x=World.mouseX;
-  movingRect.y=World.mouseY;
-  console.log(movingRect.x-fixedRect.x)
- if (movingRect.x - fixedRect.x <= fixedRect.width/2 + movingRect.width/2 && fixedRect.x-movingRect.x <= fixedRect.width/2 + movingRect.width/2
-  && movingRect.y - fixedRect.y <= fixedRect.height/2 + movingRect.height/2 && fixedRect.y-movingRect.y <= fixedRect.height/2 + movingRect.height/2) {
-    movingRect.shapeColor = "red";
-    fixedRect.shapeColor = "red"; 
-  } 
- else { movingRect.shapeColor = "green"; 
- fixedRect.shapeColor = "green"; 
-}
- drawSprites();
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching()){
+    movingRect.shapeColor = "blue";
+    fixedRect.shapeColor = "blue";
+  }
   
+  else {
+    movingRect.shapeColor = "green";
+  
+    fixedRect.shapeColor = "green"
+  }
+ 
+
+//bounceOff(movingRect,fixedRect)
+  drawSprites();
 }
+
+function isTouching(){
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2) {
+    
+    return true;
+  }
+  else {
+    return false;
+  } 
+}
+
+
